4c1e28f6b4b5558921570582cba70c63bb7c5453
nikhilsingh3112 <89055639+nikhilsingh3112@users.noreply.github.com>
2021-10-28 04:43:00 -0700

    commit

---
+++ Day 42/Ex/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Ex
+{
+    class Program  
+    {  
+        static void Main(string[] args)  
+        {  
+            Class1 cls1 = new Class1();  
+            Console.WriteLine(cls1.Method1());  
+            Console.Read();  
+        }  
+    }  
+  
+    class Class1  
+    {  
+        public String Method1()  
+        {  
+            try  
+            {  
+                Class2 cls2 = new Class2();  
+                return cls2.Method2();  
+            }  
+            catch (Exception ex)  
+            {  
+                Console.WriteLine("-----Stack Trace for the Exception Occurred------");  
+                Console.WriteLine(ex.StackTrace.ToString());  
+                Console.WriteLine("-------Method in which Exception Occurred------");  
+                Console.WriteLine(ex.TargetSite.ToString());  
+            }  
+            return "";  
+        }  
+    }  
+  
+    class Class2  
+    {  
+        public String Method2()  
+        {  
+            try  
+            {  
+                FaultyMethod2();  
+                return "Method2 called";  
+            }  
+            catch(Exception ex) {  
+                   //throw;
+                throw ex;  
+            }  
+        }  
+        //This method is responsible for the exception  
+        public void FaultyMethod2()  
+        {  
+            throw new TimeoutException();  
+        }  
+    }   
+}
