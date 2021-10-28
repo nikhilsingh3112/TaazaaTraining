31fd6dfef7cd79f605211ea665d8988668c53d2a
nikhilsingh3112 <89055639+nikhilsingh3112@users.noreply.github.com>
2021-10-28 04:43:26 -0700

    commit

---
+++ Day 43/Many_to_Many/EFCore5RelationshipsExamplesDbContext.cs
@@ -0,0 +1,16 @@
+﻿using EFCore5RelationshipsExamples.Models.ManyToMany;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore5RelationshipsExamples
+{
+    public class EFCore5RelationshipsExamplesDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;User ID=postgres;Password=1234;Database=MovieDb43;Pooling=true;");
+        }
+
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
